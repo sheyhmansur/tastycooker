dee65a922fabc5033c20b16898fb92983037a0e6
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2022-01-30 10:00:09 +0300

    stylization footer.scss done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
+  /* ----------- Инциалазиця слайдера на мобильных устройствах ---------- */
+
   const slider = document.querySelector(".info");
   const slider1 = document.querySelector(".reasons-slider");
+  const slider2 = document.querySelector(".reviews-slider");
 
   let mySwiper;
   let mySwiper2;
+  let mySwiper3;
 
-  // Инициализация слайдера на планшетных, мобильных устройствах
   function mobileSlider() {
     if (window.innerWidth <= 600 && slider.dataset.mobile == "false") {
       mySwiper = new Swiper(slider, {
@@ -58,5 +61,65 @@ window.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("resize", () => {
     mobileSlider1();
+  });
+
+  function mobileSlider2() {
+    if (window.innerWidth <= 600 && slider2.dataset.mobile == "false") {
+      mySwiper3 = new Swiper(slider2, {
+        slidesPerview: 1,
+        spaceBetween: 10,
+        loop: true,
+        slideClass: "reviews__item",
+      });
+
+      slider2.dataset.mobile = "true";
+    }
+
+    if (window.innerWidth > 600) {
+      slider2.dataset.mobile = "false";
+
+      if (slider2.classList.contains("swiper-initialized")) {
+        mySwiper3.destroy();
+      }
+    }
+  }
+
+  mobileSlider2();
+
+  window.addEventListener("resize", () => {
+    mobileSlider2();
+  });
+
+  /* ----------- Плавная прокрута до формы ---------- */
+
+  const buttons = document.querySelectorAll("[data-scroll='btn']");
+  const form = document.querySelector("#form");
+
+  const scrollTo = (element) => {
+    window.scroll({
+      left: 0,
+      top: element.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      scrollTo(form);
+    });
+  });
+
+  /* ----------- Валидация email ---------- */
+
+  const input = document.querySelector(".footer__form-input");
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  input.addEventListener("change", (e) => {
+    if (!regex.test(String(e.target.value).toLocaleLowerCase())) {
+      console.log("неправильно");
+      alert("Введите корректный еmail");
+    }
   });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+  /* ----------- Инциалазиця слайдера на мобильных устройствах ---------- */
+
   const slider = document.querySelector(".info");
   const slider1 = document.querySelector(".reasons-slider");
   const slider2 = document.querySelector(".reviews-slider");
@@ -7,7 +9,6 @@ window.addEventListener("DOMContentLoaded", () => {
   let mySwiper2;
   let mySwiper3;
 
-  // Инициализация слайдера на планшетных, мобильных устройствах
   function mobileSlider() {
     if (window.innerWidth <= 600 && slider.dataset.mobile == "false") {
       mySwiper = new Swiper(slider, {
@@ -87,5 +88,38 @@ window.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("resize", () => {
     mobileSlider2();
+  });
+
+  /* ----------- Плавная прокрута до формы ---------- */
+
+  const buttons = document.querySelectorAll("[data-scroll='btn']");
+  const form = document.querySelector("#form");
+
+  const scrollTo = (element) => {
+    window.scroll({
+      left: 0,
+      top: element.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      scrollTo(form);
+    });
+  });
+
+  /* ----------- Валидация email ---------- */
+
+  const input = document.querySelector(".footer__form-input");
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  input.addEventListener("change", (e) => {
+    if (!regex.test(String(e.target.value).toLocaleLowerCase())) {
+      console.log("неправильно");
+      alert("Введите корректный еmail");
+    }
   });
 });
